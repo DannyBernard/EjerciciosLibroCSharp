acc1f32c4f361d38ba9a0fb26ef3354967e5c296
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-02 05:58:59 +0200

    Capitulos del 1 al 4

---
+++ EjerciciodelLibroCShar/Capitulo1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciodelLibroCShar
+{
+    class Capitulo1
+    {
+        public void Ejercicio1()
+        {
+            Console.WriteLine("Danny Alberto Bernard Martinez");
+            Console.ReadKey();
+        }
+
+       public void Ejercicio2()
+        {
+            Console.WriteLine("Nombre: Danny Bernard ");
+            Console.WriteLine("Matricula: 2015-0070");
+            Console.WriteLine("Carrera: Ingenieria en Sistema");
+            Console.ReadKey();
+
+        }
+
+        public void Menu()
+        {
+           int op=0;
+            Console.WriteLine("Menu" + "\n" +
+                "1.Ejercicio_1.1\n" +
+                "2.Ejercicio_1.5" 
+                );
+            Console.WriteLine("Digite la Opcion deseada");
+            op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    {
+                   Ejercicio1();
+                    break;
+                    }
+                case 2:
+                    {
+                     Ejercicio2();
+                     break;
+                    }
+            }
+            }
+        
+            
+
+    }
+}
